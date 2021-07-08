8489a940be725f68ff119dc3dcd7e13cc0d481e9
gu jiangwei <249987839@qq.com>
2021-07-08 18:38:33 +0800

    xcode first

--- Engine/unittest/test_settings.cpp
+++ Engine/unittest/test_settings.cpp
@@ -1,6 +1,23 @@
-#include "test_settings.h"
+#include "test.h"
 #include "settings.h"
 #include "debug.h"
+
+class TestSettings:public TestBase {
+public:
+    TestSettings() { TestManager::registerTestModule(this); }
+    const char *getName() { return "TestSettings"; }
+    
+    void runTests();
+    
+    void testAllSettings();
+    void testDefaults();
+    void testFlagDesc();
+    
+    static const char *config_text_before;
+    static const std::string config_text_after;
+};
+
+static TestSettings g_test_instance;
 
 void TestSettings::runTests()
 {
